d977bcf7211b9153643342873d2c1417f8f9c7a5
TAMU-IEEE <tec@ieeetamu.org>
2016-11-08 17:09:21 -0600

    Update looping_2.py

--- Workshop 2/looping_2.py
+++ Workshop 2/looping_2.py
@@ -1,19 +1,18 @@
 ''' Write a loop that returns the sum of the next ten numbers of a seed number.
 Test Suite:
 inputs:		output:		domain:
-3		1 3 		Positive Integers
-0				Zero (border case)
--5				We won't deal with these either, lol '''
+4		99 		Positive Integers
+0		55		Zero (border case)
+-5		0		Negative numbers '''
 
 
 # Getting a number from the user
 num = int(input("Enter a number:"))
 
-# Start at 1, step by 2, go until just below (num + 1)
-for i in range(1, num + 1, 2):
-	print(i)
+sum = 0
 
-''' A more naive solution:
-for i in num:
-	if i % 2 == 1:
-		print(i) '''
+# Start at number, go until ten more than number
+for i in range(num, num + 11):
+	sum += i
+	
+print(sum)
