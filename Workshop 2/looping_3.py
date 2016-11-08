57d336f241ff13d900678a605939ec8523763b30
TAMU-IEEE <tec@ieeetamu.org>
2016-11-08 17:01:07 -0600

    Update looping_3.py

--- Workshop 2/looping_3.py
+++ Workshop 2/looping_3.py
@@ -1,7 +1,7 @@
 '''Write a loop that prints out every natural number that is divisible by 7 and less than 30.
 Test Suite:
 inputs:		output:				domain:
-none		0 7 14 21 28 		divisible by 7 and less than 30'''
+none		0 7 14 21 28 			divisible by 7 and less than 30'''
 
 
 # don't need any input, which makes this practically a calculator
